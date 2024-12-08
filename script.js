bb2eeed80133de6e81319b4ee9d923a2f929ccd4
Logikaaa <123231827+Logikaaa@users.noreply.github.com>
2024-12-08 15:59:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function getRandomSing(){
     return sing[randint(0, 2)]
 }
 
+function shuffle(array){
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex != 0){
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex --;
+        [array[currentIndex], array[randomIndex]] =
+        [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
 class Question{
     constructor(){
         let a = randint(1, 20)
@@ -26,6 +37,7 @@ class Question{
             randint(this.correct + 1, this.correct + 10),
             randint(this.correct + 1, this.correct + 10)
         ]
+        shuffle(this.answer)
     }
 
     display(){
@@ -40,3 +52,23 @@ let current_question
 current_question = new Question()
 current_question.display()
 
+for (let i = 0; i < ans_btn.length; i += 1){
+    ans_btn[i].addEventListener('click', function(){
+        if (ans_btn[i].innerHTML == current_question.correct){
+            ans_btn[i].style.background = 'green'
+            anime({
+                targets: ans_btn[i],
+                background: 'white',
+                duration: 500,
+                delay: 100,
+                easing: 'linear'
+            })
+        }
+        else{
+            ans_btn[i].style.background = 'red'
+        }
+    current_question = new Question()
+    current_question.display()
+    })
+    
+}
